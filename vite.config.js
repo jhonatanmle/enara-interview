c0f4821dc4cd2769a67cd29f5807352d5548c2f8
Jhonatan <jhonatanmorenoloayza@gmail.com>
2022-03-09 19:55:38 -0500

    feat: add project configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+const getPath = route => path.resolve(__dirname, route);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@src': getPath('./src/'),
+      '@components': getPath('./src/Components/'),
+      '@shared': getPath('./src/Shared/'),
+      '@styles': getPath('./src/styles/'),
+      '@data': getPath('./data/'),
+    },
+  },
+});
